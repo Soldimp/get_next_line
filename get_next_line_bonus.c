d507930ce2dd0086af841cda588cd35088803ab5
Nuno Manuel Garcia Guerreiro <nugarcia@c1r17s13.42lisboa.com>
2022-11-28 17:47:33 +0000

    almost done

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,35 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nugarcia < nugarcia@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/23 12:05:41 by nugarcia          #+#    #+#             */
-/*   Updated: 2022/11/28 16:54:40 by nugarcia         ###   ########.fr       */
+/*   Created: 2022/11/28 16:59:12 by nugarcia          #+#    #+#             */
+/*   Updated: 2022/11/28 17:44:13 by nugarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
+
 
 char	*get_next_line(int fd)
 {
-	static char	stash[BUFFER_SIZE + 1];
+	static char	stash[FOPEN_MAX][BUFFER_SIZE + 1];
 	char		*line;
 	int			i;
 
 	i = 0;
-	if (BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
+	if (BUFFER_SIZE < 1 || read(fd, 0, 0) < 0 || fd >= FOPEN_MAX)
 	{
-		while (stash[i])
-			stash[i++] = 0;
+		while (stash[fd][i])
+			stash[fd][i++] = 0;
 		return (NULL);
-	}	
+	}
 	line = NULL;
-	while (stash[0] || read(fd, stash, BUFFER_SIZE) > 0)
+	while (stash[fd][0] || read(fd, stash[fd], BUFFER_SIZE) > 0)
 	{
-		line = ft_strjoin(line, stash);
-		if (stashfree(stash))
+		line = ft_strjoin(line, stash[fd]);
+		if (stashfree(stash[fd]))
 			break ;
 	}
 	return (line);
@@ -40,6 +41,7 @@ char	*get_next_line(int fd)
 	int fd = 0;
 	char *to_free;
 	fd = open("./fd.txt", O_RDWR);
+	printf("%d\n", fd);
 	to_free = get_next_line(fd);
 	printf("%s", to_free);
 	close(fd);
---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nugarcia < nugarcia@student.42lisboa.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 16:58:10 by nugarcia          #+#    #+#             */
+/*   Updated: 2022/11/28 17:09:29 by nugarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 48000000
+# endif
+
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+int		stashfree(char *stash);
+#endif
